4bca06c89f002a966e363296c7fd2ce0b935dd83
Wkerlyson <wkerlyson.batista@gmail.com>
2019-10-23 13:57:52 -0300

    requesting API data

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,16 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
-void main() => runApp(MaterialApp(
-      home: Home(),
-      debugShowCheckedModeBanner: false,
-    ));
+const request =
+    "https://api.hgbrasil.com/finance?format=json-cors&key=c8c062d8";
 
-class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
-}
+void main() async {
+  Response response = await get(request);
+  print(json.decode(response.body)['results']['currencies']['USD']);
 
-class _HomeState extends State<Home> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Currency Converter'),
-      ),
-    );
-  }
+  runApp(MaterialApp(
+    home: Container(),
+  ));
 }
