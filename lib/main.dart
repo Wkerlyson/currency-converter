60d7db068e6399bacf7c43ae95039d5d89a9fa30
Wkerlyson <wkerlyson.batista@gmail.com>
2019-10-23 17:44:02 -0300

    working with Future

--- lib/main.dart
+++ lib/main.dart
@@ -7,10 +7,31 @@ const request =
     "https://api.hgbrasil.com/finance?format=json-cors&key=c8c062d8";
 
 void main() async {
-  Response response = await get(request);
-  print(json.decode(response.body)['results']['currencies']['USD']);
-
   runApp(MaterialApp(
-    home: Container(),
+    debugShowCheckedModeBanner: false,
+    home: Home(),
   ));
+}
+
+Future<Map> getData() async {
+  Response response = await get(request);
+  return json.decode(response.body);
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('\$ Converter \$'),
+        centerTitle: true,
+        backgroundColor: Colors.amber,
+      ),
+    );
+  }
 }
